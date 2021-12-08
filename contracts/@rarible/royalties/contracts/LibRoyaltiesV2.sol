587d85dcfc3a1f0dcaacc7f927c5637ad2a4e90c
bold <2985057+AboldUSER@users.noreply.github.com>
2021-12-08 14:40:40 -0800

    Update interface hash

--- contracts/@rarible/royalties/contracts/LibRoyaltiesV2.sol
+++ contracts/@rarible/royalties/contracts/LibRoyaltiesV2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 library LibRoyaltiesV2 {
     /*
-    * bytes4(keccak256('getRoyalties(LibAsset.AssetType)')) == 0x44c74bcc
+    * bytes4(keccak256('getRoyalties(LibAsset.AssetType)')) == 0xcad96cca
     */
-    bytes4 constant _INTERFACE_ID_ROYALTIES = 0x44c74bcc;
+    bytes4 constant _INTERFACE_ID_ROYALTIES = 0xcad96cca;
 }
